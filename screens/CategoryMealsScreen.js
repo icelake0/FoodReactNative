df25fa5c12ee91dea5bf272e4626810f6d52600f
Gbemileke AJIBOYE <gbemileke@seamlesshr.com>
2020-01-14 17:49:43 +0100

    create and style category-meals-page

--- screens/CategoryMealsScreen.js
+++ screens/CategoryMealsScreen.js
@@ -1,14 +1,33 @@
 import React from 'react'
-import { View, Text, StyleSheet } from 'react-native'
+import { View, Text, StyleSheet, FlatList } from 'react-native'
 import {CATEGORIES} from '../Data/dummy-data'
+import {MEALS} from '../Data/dummy-data'
+import MealItem from '../components/MealItem'
 
 const CategoryMealsScreen = props => {
     const CategoryId = props.navigation.getParam('CategoryId');
-    const category = CATEGORIES.find((category) => category.id === CategoryId)
+    const meals = MEALS.filter( (meal) => meal.categoryIds.includes(CategoryId))
+    const renderListItems = (itemData) => {
+        return (
+            <MealItem 
+                title = {itemData.item.title}
+                duration = {itemData.item.duration}
+                complexity = {itemData.item.complexity.toUpperCase()}
+                affordability = {itemData.item.affordability.toUpperCase()}
+                image = {itemData.item.imageUrl}
+                onSelect = { () => {props.navigation.navigate({
+                    routeName : 'MealDetail'
+                })} }
+            />
+        )
+    }
+    
     return (
-        <View style = {styles.screen}>
-            <Text>{category.title}</Text>
-        </View>
+        <FlatList 
+            data = {meals}
+            renderItem = {renderListItems}
+            style = {{width : "100%"}}
+        />
     )
 }
 
